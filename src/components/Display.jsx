e868ec4cdf75a420ecbbbb735e6a987eb1df6d1a
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-06-21 17:46:14 +0200

    Prueba de añadir cambio de signo

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,7 +6,7 @@ function Display(props) {
     let displayContent = props.display;
 
     return (
-        <div className="displayGeneral border border-primary">
+        <div className="displayGeneral border border-primary rounded-3">
             {displayContent}
         </div>
     )
