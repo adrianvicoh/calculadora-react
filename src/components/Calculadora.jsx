b6b7b140162923c787af5108b3b1666713ed4fd3
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-06-21 17:10:37 +0200

    Simplificada funcionalidad de calculo para comporbar funcion

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -10,50 +10,45 @@ function Calculadora() {
     const [display, setDisplay] = useState("");
     const [valorAnterior, setValorAnterior] = useState(0);
     const [operacion, setOperacion] = useState("");
-
-    let isOperating = false;
+    //const [isOperating, setIsOperating] = useState(false);
 
     function borrarDisplay() {
-        setDisplay("");
+        setDisplay("")
     }
 
     function calcular() {
-        let newOperation
+        let newOperation = 0;   
         if (operacion === "+") {
-            newOperation = valorAnterior + parseFloat(display)
+            newOperation = (valorAnterior + parseFloat(display))
             setDisplay(newOperation)
             setValorAnterior(0)
         } else if (operacion === "-") {
             newOperation = valorAnterior - parseFloat(display)
             setDisplay(newOperation)
-            setValorAnterior(0)
         } else if (operacion === "X") {
             newOperation = valorAnterior * parseFloat(display)
             setDisplay(newOperation)
-            setValorAnterior(0)
         } else if (operacion === "/") {
             if (parseFloat(display) === 0) {
                 setDisplay("ERROR")
-                setValorAnterior(0)
             } else {
                 newOperation = valorAnterior / parseFloat(display)
                 setDisplay(newOperation)
-                setValorAnterior(0)
             }
         }
     }
 
     function pulsar(input) {
         if (!isNaN(parseInt(input)) || input === ",") {
-            if (isOperating) {
-                borrarDisplay()
-                setDisplay(input)
-                isOperating = false
-            }
+            /*if (isOperating) {
+                borrarDisplay();
+                setIsOperating(false);
+            }*/
             setDisplay(display + input);
         } else if (operators.includes(input)) {
-            isOperating = true;
+            //setIsOperating(true)
             setValorAnterior(parseFloat(display))
+            borrarDisplay();
             setOperacion(input);
         } else if (input === "=") {
             calcular();
@@ -65,44 +60,41 @@ function Calculadora() {
     return (
         <>
             <div className="calcGeneral">
-                <div className="container-sm">
-                    <div className="row">
-                        <div className="col-12 border border-primary">
-                            <Display display={display} />
-                            <div className="row">
-                                <div className="col-3"><Tecla valor="AC" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="+/-" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="%" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="/" pulsar={pulsar} /></div>
-                            </div>
-                            <div className="row">
-                                <div className="col-3"><Tecla valor="7" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="8" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="9" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="X" pulsar={pulsar} /></div>
-                            </div>
-                            <div className="row">
-                                <div className="col-3"><Tecla valor="4" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="5" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="6" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="-" pulsar={pulsar} /></div>
-                            </div>
-                            <div className="row">
-                                <div className="col-3"><Tecla valor="1" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="2" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="3" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="+" pulsar={pulsar} /></div>
-                            </div>
-                            <div className="row">
-                                <div className="col-6"><Tecla valor="0" pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="," pulsar={pulsar} /></div>
-                                <div className="col-3"><Tecla valor="=" pulsar={pulsar} /></div>
-                            </div>
+                <div className="row">
+                    <div className="col-12 border border-primary">
+                        <Display display={display} />
+                        <div className="row">
+                            <div className="col-3"><Tecla valor="AC" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="+/-" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="%" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="/" pulsar={pulsar} /></div>
+                        </div>
+                        <div className="row">
+                            <div className="col-3"><Tecla valor="7" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="8" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="9" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="X" pulsar={pulsar} /></div>
+                        </div>
+                        <div className="row">
+                            <div className="col-3"><Tecla valor="4" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="5" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="6" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="-" pulsar={pulsar} /></div>
+                        </div>
+                        <div className="row">
+                            <div className="col-3"><Tecla valor="1" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="2" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="3" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="+" pulsar={pulsar} /></div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6"><Tecla valor="0" pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="," pulsar={pulsar} /></div>
+                            <div className="col-3"><Tecla valor="=" pulsar={pulsar} /></div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
